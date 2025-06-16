cd7ce53cb808c71a3476444c656bd2fbcc4e2e1a
Rebexcode <akinladerebecca04@gmail.com>
2025-06-16 20:57:21 +0100

    feat: enhance student DTO with supervisor ID and update validation for student properties

--- src/admin/dto/create-user.dto.ts
+++ src/admin/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
 
 export class CreateSupervisorDto {
   @ApiProperty({
@@ -82,13 +82,75 @@ export class CreateStudentDto {
   email: string;
 
   @ApiProperty({
-    description: 'The password for the student account',
-    example: 'student123',
+    description: 'The ID of the supervisor assigned to the student',
+    example: 1,
+    required: false,
+  })
+  @IsString()
+  @IsNotEmpty({ message: 'Supervisor ID is optional but should be a number if provided.' })
+  supervisorId?: number;
+}
+
+export class DeleteByIdDto {
+  @ApiProperty({ example: '123' })
+  @IsNotEmpty()
+  @IsNumberString()
+  id: string;
+}
+
+export class UpdateStudentDto {
+  @ApiProperty({
+    description: 'The ID of the student',
+    example: 1,
     required: true,
+  })
+  @IsNotEmpty()
+  @IsNumberString()
+  id: number;
+
+  @ApiProperty({
+    description: 'The first name of the student',
+    example: 'Jane',
+    required: true,
+    minLength: 2,
+    maxLength: 50,
   })
   @IsString()
   @IsNotEmpty()
-  password: string;
+  firstName: string;
+
+  @ApiProperty({
+    description: 'The last name of the student',
+    example: 'Smith',
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+  })
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty({
+    description: 'The matriculation number of the student',
+    example: '123456789',
+    required: true,
+    minLength: 5,
+    maxLength: 20,
+  })
+  @IsString()
+  @IsNotEmpty()
+  matricNumber: string;
+
+  @ApiProperty({
+    description: 'The email of the student',
+    example: 'janesmith@gmail.com',
+    required: true,
+    minLength: 5,
+    maxLength: 100,
+  })
+  @IsString()
+  @IsNotEmpty()
+  email: string;
 
   @ApiProperty({
     description: 'The ID of the supervisor assigned to the student',
