b9578d516681c84d11ce73e251bed85add7440c5
Rebexcode <akinladerebecca04@gmail.com>
2025-06-23 09:16:05 +0100

    fix: enable CORS in NestJS application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   const config = new DocumentBuilder().setTitle('CS Archive Backend API').setDescription('API documentation for the CS Archive Backend').setVersion('1.0').addBearerAuth().build();
   const document = SwaggerModule.createDocument(app, config);
